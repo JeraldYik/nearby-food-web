933e9cc15cade8dd88941d73a466fd19ff88444f
Jerald Yik <jeraldyik12@gmail.com>
2020-12-17 22:47:01 +0800

    added prettier & semantic ui layouts

--- src/app/.next/static/chunks/webpack.js
+++ src/app/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "86c151cc4b1bc91d9969";
+/******/ 	var hotCurrentHash = "e55c56fb3eebad412781";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
